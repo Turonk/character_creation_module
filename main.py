9a3134d6313438b2a5664755be4c44611b936bab
“Света <svetafadeeva@yahoo.com>
2022-12-03 16:46:57 +0300

    Start game

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from random import randint
 
 
+from graphic_arts.start_game_banner import run_screensaver
+
+
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
@@ -82,7 +85,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -93,7 +97,7 @@ def main():
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
-
+    
 main()
 
 
