bfdb6c5433aaa5173ef38b8ba39b7785bffa2171
Kolumbiets <kolumbiets@yandex.ru>
2022-12-16 22:29:25 +0300

    Add start_game_banner module

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
@@ -81,7 +82,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -94,3 +96,5 @@ def main() -> None:
 
 
 main()
+
+
