bad4023d1f885b22b13f614d7869e98c5fc1d74c
Dmitryi Axenov <axe.old@gmail.com>
2023-02-13 23:26:05 +0300

    changes main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from random import randint
 
 
-# from graphic_arts.start_game_banner import run_screensaver
-
 
 def attack(char_name, char_class):
     """Return: return_description."""
@@ -89,7 +87,6 @@ def choice_char_class():
 
 
 if __name__ == '__main__':
-    # run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name = input('...назови себя: ')
