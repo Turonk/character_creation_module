3cba8f44bd0db0f6b6f79cbab663efdb1870c78a
Anna <an-na1997@yandex.ru>
2023-03-14 19:36:55 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -92,15 +92,12 @@ if __name__ == '__main__':
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
-    rint(f'Здравствуй, {char_name}! '
+    print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
+    
 
-
-    # character_creation_module/main.py
-
-
-
+# character_creation_module/main.py
